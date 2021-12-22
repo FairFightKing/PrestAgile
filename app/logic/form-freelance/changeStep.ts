a9a5d1c861536d4ed8a393dfb14cb0cd922069c7
julesdayaux <Pwrb5ed5>
2021-12-22 16:40:17 +0100

    UPDATE changestep in interface and interface implementation

--- app/logic/form-freelance/changeStep.ts
+++ app/logic/form-freelance/changeStep.ts
@@ -1,7 +1,13 @@
 import Step from "./Step";
 
-export const changeStep = (value, array) => {
-    return new Step(array.Etapes[value].id, array.Etapes[value].title, array.Etapes[value].input)
+export interface StepGeneratorInterface {
+    changeStep(value: number, array: Array<Object>): Step;
+}
+
+export class StepGeneratorImplementation implements StepGeneratorInterface {
+    changeStep(value, array): Step {
+        return new Step(array.Etapes[value].id, array.Etapes[value].title, array.Etapes[value].input)
+    }
 }
 
 
--- app/tests/form-freelance/essai.test.ts
+++ app/tests/form-freelance/essai.test.ts
@@ -1,4 +1,4 @@
-import {changeStep} from "../../logic/form-freelance/changeStep";
+import {StepGeneratorImplementation} from "../../logic/form-freelance/changeStep";
 
 const data = {
     Etapes: [
@@ -23,35 +23,14 @@ const data = {
                     multipleChoice: false
                 }
             ]
-        },
-        {
-            id: 2,
-            title: "Etape 1",
-            input: [
-                {
-                    id: 1,
-                    name: "text",
-                    information: "Merci de pas faire de fautes",
-                    additionnalData: null,
-                    type: "text",
-                    multipleChoice: false
-                },
-                {
-                    id: 2,
-                    name: "Second",
-                    information: "oui",
-                    additionnalData: "competences",
-                    type: "select",
-                    multipleChoice: false
-                }
-            ]
         }
     ]
 }
 
 test('should return the new array with new step', () => {
-    let currentCase = 1;
-    expect(changeStep(currentCase, data)).toEqual(
+    let currentCase = 0;
+    let StepGeneratorImpl = new StepGeneratorImplementation();
+    expect(StepGeneratorImpl.changeStep(currentCase, data)).toEqual(
         expect.objectContaining({id: currentCase + 1})
     )
 });
