62c159cfc9a3376abe443b9bda2f7cd1b622c036
julesdayaux <Pwrb5ed5>
2021-12-22 14:45:18 +0100

    UPDATE numberinputappprops useless nullable types

--- app/ui/components/form/input/types/NumberInputAppProps.ts
+++ app/ui/components/form/input/types/NumberInputAppProps.ts
@@ -1,7 +1,7 @@
 export declare interface NumberInputAppProps {
     valueDefault ?: number;
-    minimum ?: number;
-    maximum ?: number;
+    minimum: number;
+    maximum: number;
     steps ?: number;
     sized ?: string
 }
