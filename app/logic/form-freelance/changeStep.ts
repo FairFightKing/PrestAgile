c107cbada81d6f96505bc082fe8861c68628a71f
julesgabriel <49538830+julesgabriel@users.noreply.github.com>
2021-12-22 20:11:41 +0100

    Update app/logic/form-freelance/changeStep.ts
    Lisibility improvement
    
    Co-authored-by: Sosthène Fruchard <43240639+FairFightKing@users.noreply.github.com>

--- app/logic/form-freelance/changeStep.ts
+++ app/logic/form-freelance/changeStep.ts
@@ -5,7 +5,11 @@ export interface StepGeneratorInterface {
 }
 
 export class StepGeneratorImplementation implements StepGeneratorInterface {
-    changeStep = (value, array): Step => new Step(array.Etapes[value].id, array.Etapes[value].title, array.Etapes[value].input)
+    changeStep = (value, array): Step => new Step(
+        array.Etapes[value].id,
+        array.Etapes[value].title,
+        array.Etapes[value].input
+    )
 }
 
 
