e5d094e601fa3a8db33ee910c50b1717c8c567c7
julesdayaux <Pwrb5ed5>
2022-01-06 10:18:53 +0100

    REFACTOR archi for validator and fix hash password test, was'nt working

--- api/src/UserManager/LoginRegister/Test/fetchInput.test.ts
+++ api/src/UserManager/LoginRegister/Test/fetchInput.test.ts
@@ -1,5 +1,6 @@
 import {expect} from "@jest/globals";
 import * as bcrypt from 'bcrypt';
+import FormInputValidator from "../../../../../logic/validators/registration/formInputValidator";
 
 export interface ILoginForm {
     email: string,
@@ -26,26 +27,6 @@ const falseDataPassword: ILoginForm = {
 //     checkInputPassword(password:ILoginForm["password"]) : boolean
 // }
 
-class InputValidator {
-    public static checkInputFields = ({email, password}: ILoginForm): ILoginForm => {
-        if (!this.checkInputEmail(email)) throw Error('The User Email is not valid');
-        if (!this.checkInputPassword(password)) throw Error('The User Password is not valid');
-        return {email: email, password: password};
-    }
-    private static checkInputEmail = (email: ILoginForm["email"]): boolean => {
-        // todo : check length  >= 11
-        const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        return email.length >= 11 &&
-            regexp.test(email)
-            ;
-    }
-    private static checkInputPassword = (password: ILoginForm["password"]): boolean => {
-        // todo : Make regex allow all special characters
-        const regexp = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?_&])[A-Za-z\d@$!%*?&]{8,}$/);
-        return regexp.test(password);
-    }
-}
-
 class PasswordHelper {
     public static hash = (password: ILoginForm["password"]): Promise<ILoginForm["password"]> => {
         return bcrypt.hash(password, 10)
@@ -58,10 +39,10 @@ class PasswordHelper {
 /* ------------------------------------------------------------------------------------------------------------------ */
 
 
-test('The input fields should be verified', () => expect(InputValidator.checkInputFields(data)).toBeTruthy() )
-test('Wrong Email should throw custom error', () => expect(() => InputValidator.checkInputFields(falseData)).toThrow() )
-test('Wrong password should throw custom error', () => expect(() => InputValidator.checkInputFields(falseDataPassword)).toThrow() )
-test('Password hashed successfully', () => expect(PasswordHelper.hash(InputValidator.checkInputFields(data).password)) )
+test('The input fields should be verified', () => expect(FormInputValidator.checkInputFields(data)).toBeTruthy() )
+test('Wrong Email should throw custom error', () => expect(() => FormInputValidator.checkInputFields(falseData)).toThrow() )
+test('Wrong password should throw custom error', () => expect(() => FormInputValidator.checkInputFields(falseDataPassword)).toThrow() )
+test('Password hashed successfully', async () => expect(await PasswordHelper.hash(FormInputValidator.checkInputFields(data).password)) )
 test('Hash password can be validated', async () => {
     const compare = await PasswordHelper.hash(data.password);
     expect(await PasswordHelper.compare(data.password, compare)).toBeTruthy();
