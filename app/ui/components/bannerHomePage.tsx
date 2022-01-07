a36775923da7855d55d3303f2eb3ea351c664e92
Alexandre <alexfont@hotmail.fr>
2022-01-07 10:23:12 +0100

    ADD clip path 2&3 ADD png for clip path template UPDATE clip path 1

--- app/ui/components/bannerHomePage.tsx
+++ app/ui/components/bannerHomePage.tsx
@@ -2,8 +2,8 @@ import { Box, Flex, Button, ButtonGroup, Text, Image } from '@chakra-ui/react';
 
 export default function BannerHomePage() {
   return (
-    <Flex bg="grey" w="100%">
-      <Box bg="#2945FF" w="100%" color="white">
+    <Flex bg="grey" h="520px" w="100%">
+      <Box bg="#2945FF" w="50%" color="white">
         <Box my={75} ml={125}>
           <Text
             lineHeight="120%"
@@ -58,7 +58,55 @@ export default function BannerHomePage() {
           </ButtonGroup>
         </Box>
       </Box>
-      <Box className="test"></Box>
+      <Flex bg="red" w="50%">
+        <Box
+          zIndex={7}
+          w="100%"
+          clipPath="polygon(
+    0% 0%,
+    0% 100%,
+    100% 100%,
+    80% 96.5%,
+    75% 95%,
+    70% 93%,
+    65% 91%,
+    60% 88.5%,
+    55% 85.5%,
+    50% 81.5%,
+    45% 77%,
+    40% 72%,
+    34.5% 65%,
+    31% 60%,
+    25% 51%,
+    20% 42%,
+    15.5% 33%,
+    13.95% 30%,
+    12% 26%,
+    9.75% 21.5%,
+    7.25% 16%,
+    1.5% 4.5%,
+    0.75% 2.5%
+  )"
+          h="100%"
+          bg="#2945ff"
+        ></Box>
+        <Box
+          zIndex={6}
+          w="40%"
+          h="40%"
+          bg="#E1DCFF"
+          mx="-100%"
+          clipPath="polygon(0% 0%, 10% 40%, 25% 33%, 31% 31%, 38% 30%, 44% 30%, 53% 31%, 63% 31%, 70% 31%, 77% 30%, 88% 25%, 91% 23%, 96% 19%, 98% 15%, 100% 9%, 100% 0%, 87% 0%);"
+        ></Box>
+        <Box
+          zIndex={5}
+          w="25%"
+          h="70%"
+          bg="#FFEEEE"
+          mx="60%"
+          clipPath="polygon(0% 0%, 0% 100%, 100% 100%, 92% 84%, 90% 79%, 89% 72%, 85% 58%, 84% 46%, 84% 42%, 87% 27%, 93% 9%, 100% 0%);"
+        ></Box>
+      </Flex>
     </Flex>
   );
 }
