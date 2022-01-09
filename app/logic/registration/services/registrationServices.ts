d47d4a59ca0d3dd61507fd99bae371ba3c6b26dc
julesdayaux <Pwrb5ed5>
2022-01-09 17:44:18 +0100

    UPDATE optimisation of const

--- app/logic/registration/services/registrationServices.ts
+++ app/logic/registration/services/registrationServices.ts
@@ -5,11 +5,11 @@ interface RegistrationInput {
 
 export class RegistrationServicesImpl {
     public static checkInputForApi = ({email, password}: RegistrationInput): boolean => {
-        const emailRegex = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/
-        const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
         if (!(password.length >= 8)) return false
         if (!(/[A-Z]/.test(password))) return false
+        const emailRegex = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/
         if (!(emailRegex.test(email))) return false
+        const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
         return specialCharRegex.test(password);
     }
 }
