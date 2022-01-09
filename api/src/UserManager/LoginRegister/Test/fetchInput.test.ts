eea7266a74f15e72488765ded889bbec50ca3c9e
FairFightKing <sosthenefruchard@gmail.com>
2022-01-09 13:57:49 +0100

    UPD[noBehavior] eslint prettier setup + applied

--- api/src/UserManager/LoginRegister/Test/fetchInput.test.ts
+++ api/src/UserManager/LoginRegister/Test/fetchInput.test.ts
@@ -1,30 +1,40 @@
-import {expect} from "@jest/globals";
-import FormInputValidator from "../../../../../logic/validators/registration/formInputValidator";
-import PasswordHelper from "../../helpers/PasswordHelper";
-import {LoginFormDTO} from "../DTO/LoginFormDTO";
+import { expect } from '@jest/globals'
+import FormInputValidatorImpl from '../Validator/Impl/formInputValidatorImpl'
+import PasswordHelper from '../../helpers/PasswordHelper'
+import { LoginFormDTO } from '../DTO/LoginFormDTO'
 
 const data: LoginFormDTO = {
-    email: 'sosthene.fruchard78@gmail.com',
-    password: 'Password@7889',
+  email: 'sosthene.fruchard78@gmail.com',
+  password: 'Password@7889',
 }
 const falseData: LoginFormDTO = {
-    email: 'soenefm',
-    password: 'Password@7889',
+  email: 'soenefm',
+  password: 'Password@7889',
 }
 const falseDataPassword: LoginFormDTO = {
-    email: 'sosthene.fruchard78@gmail.com',
-    password: 'prd',
+  email: 'sosthene.fruchard78@gmail.com',
+  password: 'prd',
 }
 
-test('The input fields should be verified', () => expect(FormInputValidator.checkInputFields(data)).toBeTruthy() )
-test('Wrong Email should throw custom error', () => expect(() => FormInputValidator.checkInputFields(falseData)).toThrow() )
-test('Wrong password should throw custom error', () => expect(() => FormInputValidator.checkInputFields(falseDataPassword)).toThrow() )
-test('Password hashed successfully', async () => expect(await PasswordHelper.hash(FormInputValidator.checkInputFields(data).password)) )
+test('The input fields should be verified', () =>
+  expect(FormInputValidatorImpl.checkInputFields(data)).toBeTruthy())
+test('Wrong Email should throw custom error', () =>
+  expect(() => FormInputValidatorImpl.checkInputFields(falseData)).toThrow())
+test('Wrong password should throw custom error', () =>
+  expect(() =>
+    FormInputValidatorImpl.checkInputFields(falseDataPassword),
+  ).toThrow())
+test('Password hashed successfully', async () =>
+  expect(
+    await PasswordHelper.hash(
+      FormInputValidatorImpl.checkInputFields(data).password,
+    ),
+  ))
 test('Hash password can be validated', async () => {
-    const compare = await PasswordHelper.hash(data.password);
-    expect(await PasswordHelper.compare(data.password, compare)).toBeTruthy();
+  const compare = await PasswordHelper.hash(data.password)
+  expect(await PasswordHelper.compare(data.password, compare)).toBeTruthy()
 })
 test('Hash password will return false when data arent equal', async () => {
-    const compare = await PasswordHelper.hash(falseDataPassword.password);
-    expect(await PasswordHelper.compare(data.password, compare)).toBeFalsy();
+  const compare = await PasswordHelper.hash(falseDataPassword.password)
+  expect(await PasswordHelper.compare(data.password, compare)).toBeFalsy()
 })
--- api/src/UserManager/helpers/PasswordHelper.ts
+++ api/src/UserManager/helpers/PasswordHelper.ts
@@ -1,12 +1,19 @@
-import * as bcrypt from "bcrypt";
-import {LoginFormDTO} from "../LoginRegister/DTO/LoginFormDTO";
-
+import * as bcrypt from 'bcrypt'
+import { LoginFormDTO } from '../LoginRegister/DTO/LoginFormDTO'
 
 export default class PasswordHelper {
-    public static hash = (password: LoginFormDTO["password"]): Promise<LoginFormDTO["password"]> => {
-        return bcrypt.hash(password, 10)
-    }
-    public static compare = (passwordToCompareTo: LoginFormDTO["password"], passwordInDb: LoginFormDTO["password"]): Promise<boolean> => {
-        return bcrypt.compare(passwordToCompareTo, passwordInDb)
-    }
+  public static hash = (
+    password: LoginFormDTO['password'],
+  ): Promise<LoginFormDTO['password']> => {
+    if (password === undefined) throw Error
+    return bcrypt.hash(password, 10)
+  }
+  public static compare = (
+    passwordToCompareTo: LoginFormDTO['password'] | Buffer,
+    passwordInDb: LoginFormDTO['password'],
+  ): Promise<boolean> => {
+    if (passwordToCompareTo === undefined || passwordInDb === undefined)
+      throw Error
+    return bcrypt.compare(passwordToCompareTo, passwordInDb)
+  }
 }
