aa4f154d64f5623fa0e53206ddfd26f6cf8bb4b8
julesdayaux <Pwrb5ed5>
2022-01-09 17:02:18 +0100

    UPDATE test

--- app/logic/registration/tests/unitary-tests/registration.test.ts
+++ app/logic/registration/tests/unitary-tests/registration.test.ts
@@ -4,12 +4,5 @@ import {RegistrationServicesImpl} from "../../services/registrationServices";
 test('expect object to be validated', () => {
     const email = "julesgabriel.dayaux@gmail.com";
     const password = "Pkui567@flkiu";
-    expect(RegistrationServicesImpl.checkInputForDisplay({email, password})).toEqual({
-        email: true,
-        password: {
-            specialChar: true,
-            length: true,
-            uppercase: true
-        }
-    })
+    expect(RegistrationServicesImpl.checkInputForApi({email, password})).toBeTruthy()
 })
