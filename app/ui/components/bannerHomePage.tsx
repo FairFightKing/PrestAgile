13be222a8116fee8b13b12674614202d4ab6fe98
Alexandre <alexfont@hotmail.fr>
2022-01-10 10:01:38 +0100

    UPDATE image size & UPDATE bannerComponent

--- app/ui/components/bannerHomePage.tsx
+++ app/ui/components/bannerHomePage.tsx
@@ -4,7 +4,7 @@ export default function BannerHomePage() {
   return (
     <Flex h="520px">
       <Box bg="#2945FF" w="50%" color="white">
-        <Box my="12.5%" ml="20%">
+        <Box my="7.5%" ml="20%">
           <Text
             lineHeight="120%"
             my="2%"
@@ -46,8 +46,11 @@ export default function BannerHomePage() {
           </ButtonGroup>
         </Box>
       </Box>
-      <Flex w="50%">
+      <Box w="50%" position="static" top="0px" left="0px">
         <Box
+          position="relative"
+          top="0px"
+          left="0px"
           zIndex={7}
           w="100%"
           clipPath="polygon(0% 0%,0% 100%,100% 100%,80% 96.5%,75% 95%,70% 93%,65% 91%,60% 88.5%,55% 85.5%,50% 81.5%,45% 77%,40% 72%,34.5% 65%,31% 60%,25% 51%,20% 42%,15.5% 33%,13.95% 30%,12% 26%,9.75% 21.5%,7.25% 16%,1.5% 4.5%,0.75% 2.5%)"
@@ -56,10 +59,12 @@ export default function BannerHomePage() {
         />
         <Box
           zIndex={6}
+          position="relative"
           w="40%"
           h="40%"
           bg="#E1DCFF"
-          mx="-100%"
+          top="-520px"
+          left="0px"
           clipPath="polygon(0% 0%, 10% 40%, 25% 33%, 31% 31%, 38% 30%, 44% 30%, 53% 31%, 63% 31%, 70% 31%, 77% 30%, 88% 25%, 91% 23%, 96% 19%, 98% 15%, 100% 9%, 100% 0%, 87% 0%);"
         />
         <Box
@@ -67,16 +72,20 @@ export default function BannerHomePage() {
           w="25%"
           h="70%"
           bg="#FFEEEE"
-          mx="60%"
+          position="relative"
+          top="-680px"
+          left="0px"
           clipPath="polygon(0% 0%, 0% 100%, 100% 100%, 92% 84%, 90% 79%, 89% 72%, 85% 58%, 84% 46%, 84% 42%, 87% 27%, 93% 9%, 100% 0%);"
         />
-        <Box mx="-64.15%">
-          <Image
-            alt="2 personnes qui travaillent ensemble"
-            src="bannerHomePagePicture.png"
-          />
-        </Box>
-      </Flex>
+        <Image
+          height="100%"
+          position="relative"
+          top="-1092px"
+          left="0px"
+          alt="2 personnes qui travaillent ensemble"
+          src="bannerHomePagePicture.png"
+        />
+      </Box>
     </Flex>
   );
 }
