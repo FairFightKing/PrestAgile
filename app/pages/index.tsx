4b9c4f34ed297f8e0b87ba2468919bd78c0a3737
Alexandre <alexfont@hotmail.fr>
2022-01-13 16:00:11 +0100

    ADD picture in grid && ADD grid code

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Layout from '../ui/components/layout';
 import BannerHomePage from '../ui/components/bannerHomePage';
 import RecruitmentHomePage from '../ui/components/recruitmentHomePage';
+import CategoryHomePage from '../ui/components/categoryHomePage';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
       Page accueil Next Js.
       <BannerHomePage />
       <RecruitmentHomePage />
+      <CategoryHomePage />
     </div>
   );
 };
