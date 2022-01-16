2ec33df0c5bad456aa153ff5088baca6d23fe9c8
Alexandre <alexfont@hotmail.fr>
2022-01-16 09:58:46 +0100

    ADD GridRecruitementHomePageApp

--- app/ui/components/recruitement/types/GridRecruitementHomepageApp.ts
+++ app/ui/components/recruitement/types/GridRecruitementHomepageApp.ts
@@ -1,21 +1,20 @@
 export declare interface GridRecruitementHomepageProps {
-    gridPadding : string
-    gridWidth: string
-    gridHeight: string
-    imgAlt: string
-    imgSrc: string
-    imgJustifyItems: string
-    imgAlignItems: string
-    textTitleFontSize: string
-    textTitleFontFamily: string
-    textTitleFontWeight: string
-    textTitleLineHeight: string
-    textTitlePaddingY: string
-    textContentTitle: string
-    textFontSize: string
-    textFontFamily: string
-    textFontWeight: string
-    textLineHeight: string
-    textContent: string
-
+  gridPadding: string;
+  gridWidth: string;
+  gridHeight: string;
+  imgAlt: string;
+  imgSrc: string;
+  imgJustifyItems: string;
+  imgAlignItems: string;
+  textTitleFontSize: string;
+  textTitleFontFamily: string;
+  textTitleFontWeight: string;
+  textTitleLineHeight: string;
+  textTitlePaddingY: string;
+  textContentTitle: string;
+  textFontSize: string;
+  textFontFamily: string;
+  textFontWeight: string;
+  textLineHeight: string;
+  textContent: string;
 }
