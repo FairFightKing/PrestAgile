1245b08d1dab0030abf130e969636b01aaedf4df
julesgabriel <julesgabriel.dayaux@gmail.com>
2022-01-17 11:48:54 +0100

    UPDATE register atomic design

--- app/logic/registration/services/registrationServices.ts
+++ app/logic/registration/services/registrationServices.ts
@@ -1,35 +1,37 @@
-import axios, {AxiosResponse} from "axios";
+import axios, { AxiosResponse } from 'axios';
 
 export interface RegistrationInput {
-    email: string;
-    password: string;
-    userInfo?: userInfo
+  email: string;
+  password: string;
+  userInfo?: userInfo;
 }
 
 interface userInfo {
-    firstName: string
-    lastName: string
-    phone?: number
+  firstName: string;
+  lastName: string;
+  phone?: number;
 }
 
 export class RegistrationServicesImpl {
-    public static checkInputForApi = ({
-                                          email,
-                                          password,
-                                      }: RegistrationInput): boolean => {
-        if (!(password.length >= 8)) return false;
-        if (!/[A-Z]/.test(password)) return false;
-        const emailRegex =
-            /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
-        if (!emailRegex.test(email)) return false;
-        const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-        return specialCharRegex.test(password);
-    };
+  public static checkInputForApi = ({
+    email,
+    password,
+  }: RegistrationInput): boolean => {
+    if (!(password.length >= 8)) return false;
+    if (!/[A-Z]/.test(password)) return false;
+    const emailRegex =
+      /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
+    if (!emailRegex.test(email)) return false;
+    const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    return specialCharRegex.test(password);
+  };
 
-    public static sendDataToApi = (data: RegistrationInput): Promise<AxiosResponse<any>> => {
-        let response = axios.post("http://localhost:8000/api/auth/register", data)
-        return response.then(res => {
-            return res.data
-        })
-    }
+  public static sendDataToApi = (
+    data: RegistrationInput,
+  ): Promise<AxiosResponse<any>> => {
+    let response = axios.post('http://localhost:8000/api/auth/register', data);
+    return response.then(res => {
+      return res.data;
+    });
+  };
 }
---
+++ app/ui/components/register/FormRegister.tsx
@@ -0,0 +1,45 @@
+import { Heading, Text } from '@chakra-ui/react';
+import { Input } from '@chakra-ui/input';
+import { ValidationContainer } from './ValidationContainer';
+
+export default function FormRegister({ submit, change, data }) {
+  return (
+    <form onSubmit={submit} onChange={change}>
+      <Heading size="xl" m="1rem 0">
+        Valide ton inscription
+      </Heading>
+      <Text as="label" htmlFor="firstName">
+        Entrez votre prénom
+      </Text>
+      <Input type="text" id="firstName" />
+      <Text as="label" htmlFor="lastName">
+        Entrez votre nom
+      </Text>
+      <Input type="text" id="lastName" />
+      <Text as="label" htmlFor="email">
+        Entrez votre email
+      </Text>
+      <Input type="text" id="email" />
+      <Text as="label" htmlFor="password">
+        Entrez votre mot de passe
+      </Text>
+      <Input type="password" id="password" />
+      <Text as="label" htmlFor="confirmPassword">
+        Confirmez votre mot de passe
+      </Text>
+      <Input m="0 0 1rem 0" type="password" id="confirmPassword" />
+      <br />
+      <ValidationContainer data={data} />
+      <br />
+      <Text as="label" htmlFor="submission" />
+      <Input
+        fontSize="1rem"
+        backgroundColor="#2945FF"
+        color="white"
+        width="50%"
+        type="submit"
+        value="Valider mon inscription"
+      />
+    </form>
+  );
+}
