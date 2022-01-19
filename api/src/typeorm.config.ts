b3d3fae14beb47664faaaff6eed58f4e2607a33b
FairFightKing <sosthenefruchard@gmail.com>
2022-01-19 15:19:57 +0100

    ADD CI config

--- api/src/typeorm.config.ts
+++ api/src/typeorm.config.ts
@@ -4,14 +4,19 @@ import { config } from 'dotenv'
 
 config()
 const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: process.env.DB_NAME,
+  type: process.env.DB_TYPE as any,
+  database: process.env.Database,
+  host: process.env.Host,
+  port: parseInt(process.env.Port),
+  username: process.env.User,
+  password: process.env.Password,
   entities: [join(__dirname, '**', 'Entity/*.{ts,js}')],
   autoLoadEntities: true,
   synchronize: true,
   migrationsRun: false,
   migrations: ['src/migration/*{.ts,.js}'],
   cli: {
+    entitiesDir: 'src/**/Entity/*.{ts,js}',
     migrationsDir: 'src/migration',
   },
 }
