dc10cc9b756062d711bdb07f0d1cb8f2370ffd65
alexandre1811 <alexfont@hotmail.fr>
2022-07-27 16:56:38 +0200

    UPDATE  Header Connexion Button

--- app/ui/components/headerConnection.tsx
+++ app/ui/components/headerConnection.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Spacer, Image, Text } from '@chakra-ui/react'
+import { Box, Flex, Spacer, Image, Text, Button } from '@chakra-ui/react'
 import { LinkComponent } from './navigation/LinkComponent'
 
 export default function HeaderNoConnection() {
@@ -18,6 +18,46 @@ export default function HeaderNoConnection() {
         />
       </Box>
       <Spacer />
+      <Box my={5}>
+        <Button
+          mx={5}
+          my={{ base: '2', md: '0', lg: '0' }}
+          py={4}
+          fontSize="15px"
+          fontWeight={600}
+          px={6}
+          size="lg"
+          borderRadius="100px"
+        >
+          Tableau de bord
+        </Button>
+        <Button
+          mx={5}
+          my={{ base: '2', md: '0', lg: '0' }}
+          py={4}
+          fontSize="15px"
+          fontWeight={600}
+          px={6}
+          borderRadius="100px"
+          size="lg"
+        >
+          Mes missions
+        </Button>
+        <Button
+          mx={5}
+          my={{ base: '2', md: '0', lg: '0' }}
+          py={4}
+          fontSize="15px"
+          fontWeight={600}
+          px={6}
+          size="lg"
+          borderRadius="100px"
+          bg="#642EFF"
+          color="white"
+        >
+          Déconexion
+        </Button>
+      </Box>
     </Flex>
   )
 }
--- app/ui/components/layout.tsx
+++ app/ui/components/layout.tsx
@@ -1,10 +1,12 @@
 import HeaderNoConnection from './headerNoConnection'
+import HeaderConnection from './headerConnection'
 import Footer from './footer'
 
 function Layout({ children }: any) {
   return (
     <>
       <HeaderNoConnection />
+      <HeaderConnection />
       <main>{children}</main>
       <Footer />
     </>
