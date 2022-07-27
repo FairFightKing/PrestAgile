6e303b1af7d0b006a1ad1fe960b6f2719f75b301
alexandre1811 <alexfont@hotmail.fr>
2022-07-27 16:10:13 +0200

    UPDATE Header Logo Home Button

--- app/ui/components/header.tsx
+++ app/ui/components/header.tsx
@@ -1,16 +1,22 @@
 import TotalButtonHeader from './buttonHomePage/totalButtonHeader'
 import { Box, Flex, Spacer, Image, Text } from '@chakra-ui/react'
+import { LinkComponent } from './navigation/LinkComponent'
 
 export default function Header() {
   return (
     <Flex fontFamily="Inter" height="auto" width="100%">
       <Box textAlign="center" py={4} px={5}>
-        <Box px={5}>
-          <Image alt="Logo Prestagile" src="PA-logo.png" />
-          <Text fontSize="lg" fontWeight="600">
-            Prestagile
-          </Text>
-        </Box>
+        <LinkComponent
+          child={
+            <Box px={5}>
+              <Image alt="Logo Prestagile" src="PA-logo.png" />
+              <Text fontSize="lg" fontWeight="600">
+                Prestagile
+              </Text>
+            </Box>
+          }
+          target="/"
+        />
       </Box>
       <Spacer />
       <TotalButtonHeader />
